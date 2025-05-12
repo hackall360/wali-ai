eb676831ecede5ee91ff7b576ce6db7c565b13bb
glazk0 <43091603+glazk0@users.noreply.github.com>
2025-05-12 11:03:41 +0200

    feat: item command

---
+++ src/commands/dune/item.ts
@@ -0,0 +1,121 @@
+import {
+  ActionRowBuilder,
+  ApplicationCommandOptionType,
+  AutocompleteInteraction,
+  ButtonBuilder,
+  ButtonStyle,
+  CommandInteraction,
+  EmbedBuilder,
+  hyperlink,
+  unorderedList,
+  type APIEmbedField
+} from 'discord.js';
+
+import { config } from '#config';
+import { Command } from '#models/command';
+import { type ItemModel } from '#types/database';
+import { api } from '#utils/api';
+import { DATABASE_URL, truncateArray } from '#utils/common';
+
+export default new (class extends Command {
+  constructor() {
+    super({
+      name: 'item',
+      description: 'Returns the details of the specified item',
+      options: [
+        {
+          name: 'name',
+          description: 'Name of the item',
+          type: ApplicationCommandOptionType.String,
+          required: true,
+          autocomplete: true
+        }
+      ]
+    });
+  }
+
+  override async execute(interaction: CommandInteraction): Promise<void> {
+    if (!interaction.isChatInputCommand()) return;
+
+    await interaction.deferReply();
+
+    const name = interaction.options.getString('name', true);
+
+    const data = await api.get<ItemModel>(name);
+
+    if (!data) {
+      interaction.reply(`The item "${name}" could not be found.`);
+      return;
+    }
+
+    const actionRow = new ActionRowBuilder<ButtonBuilder>();
+
+    const embed = new EmbedBuilder();
+
+    embed.setColor(0xf3deb5);
+
+    if (data.name) {
+      embed.setTitle(data.name);
+      embed.setURL(`${DATABASE_URL}/items/${data.id}`);
+    }
+
+    if (data.description) {
+      embed.setDescription(data.description);
+    }
+
+    if (data.iconPath) {
+      embed.setThumbnail(`${config.cdnUrl}${data.iconPath.replace('{height}', '256')}`);
+    }
+
+    if (data.dropLocations?.length) {
+      actionRow.addComponents(
+        new ButtonBuilder({
+          label: 'View locations',
+          style: ButtonStyle.Link,
+          url: `${DATABASE_URL}/items/${data.id}`,
+        }),
+      );
+    }
+
+    const fields: APIEmbedField[] = [];
+
+    // TODO: Add stats & other data
+
+    if (data.requiredForContract?.length) {
+      const contracts = data.requiredForContract.map((contract) => {
+        if (!contract?.name) return 'Unknown';
+        return hyperlink(contract.name ?? 'Unknown', `${DATABASE_URL}/contracts/${contract.id}`)
+      });
+
+      fields.push({
+        name: 'Related Contracts',
+        value: unorderedList(truncateArray(contracts, 5)),
+      });
+    }
+
+    embed.addFields(fields)
+
+    await interaction.editReply({
+      embeds: [embed],
+      components: actionRow.components.length ? [actionRow] : []
+    });
+  }
+
+  override async autocomplete(interaction: AutocompleteInteraction): Promise<void> {
+    const value = interaction.options.getFocused();
+
+    // TODO: Add type checking
+    let data = await api.search(value, ["Items", "Misc", "Weapons", "Utilty", "Vehicles", "Garment", "Contract"])
+
+    data = data.slice(0, 25);
+
+    await interaction.respond(
+      data
+        .filter((entry) => entry.name !== undefined && entry.path !== undefined)
+        .map((entry) => ({
+          name: entry.name as string,
+          value: entry.path as string
+        }))
+    );
+  }
+})();
