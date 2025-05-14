eb11d23252a5284a7f53ea47546a638771b9024d
glazk0 <43091603+glazk0@users.noreply.github.com>
2025-05-14 19:44:41 +0200

    feat(commands): skill command

---
+++ src/commands/dune/skill.ts
@@ -0,0 +1,185 @@
+import {
+  type APIEmbedField,
+  ActionRowBuilder,
+  ApplicationCommandOptionType,
+  AutocompleteInteraction,
+  ButtonBuilder,
+  ButtonStyle,
+  CommandInteraction,
+  subtext,
+  unorderedList,
+} from 'discord.js';
+
+import { config } from '#config';
+import { Command } from '#models/command';
+import { Embed } from '#models/embed';
+import { type SkillAttributeBonusModel, type SkillModel } from '#types/database';
+import { api } from '#utils/api';
+import { DATABASE_URL } from '#utils/common';
+
+export default new (class extends Command {
+  constructor() {
+    super({
+      name: 'skill',
+      description: 'Returns the details of the specified skill',
+      options: [
+        {
+          name: 'name',
+          description: 'Name of the skill',
+          type: ApplicationCommandOptionType.String,
+          required: true,
+          autocomplete: true,
+        },
+      ],
+    });
+  }
+
+  override async execute(interaction: CommandInteraction): Promise<void> {
+    if (!interaction.isChatInputCommand()) return;
+
+    await interaction.deferReply();
+
+    const name = interaction.options.getString('name', true);
+
+    const data = await api.get<SkillModel>(name);
+
+    if (!data) {
+      interaction.reply(`The skill "${name}" could not be found.`);
+      return;
+    }
+
+    const actionRow = new ActionRowBuilder<ButtonBuilder>({
+      components: [
+        new ButtonBuilder({
+          label: 'Try the Skill Builder',
+          style: ButtonStyle.Link,
+          url: `${DATABASE_URL}/skill-builder`,
+        }),
+      ],
+    });
+
+    const embed = new Embed();
+
+    if (data.name) {
+      embed.setTitle(data.name);
+      embed.setURL(`${DATABASE_URL}/skills/${data.id}`);
+    }
+
+    if (data.description) {
+      embed.setDescription(data.description);
+    }
+
+    if (data.iconPath) {
+      embed.setThumbnail(`${config.cdnUrl}${data.iconPath}`);
+    }
+
+    const fields: APIEmbedField[] = [];
+
+    if (data.skillTree) {
+      fields.push({
+        name: 'Skill Tree',
+        value: data.skillTree,
+        inline: true,
+      });
+    }
+
+    if (data.skillType) {
+      fields.push({
+        name: 'Skill Type',
+        value: data.skillType,
+        inline: true,
+      });
+    }
+
+    if (data.maxLevel) {
+      fields.push({
+        name: 'Max Level',
+        value: data.maxLevel.toString(),
+        inline: true,
+      });
+    }
+
+    if (data.attributeBonuses?.length) {
+      const attributeBonuses =
+        data.attributeBonuses?.reduce((acc: Record<number, SkillAttributeBonusModel[]>, attribute) => {
+          if (attribute.level !== undefined) {
+            if (!acc[attribute.level]) {
+              acc[attribute.level] = [];
+            }
+            (acc[attribute.level] = acc[attribute.level] || []).push(attribute);
+          }
+          return acc;
+        }, {}) || {};
+
+      for (const level of Object.keys(attributeBonuses).map(Number)) {
+        const attributes = attributeBonuses[level];
+
+        if (!attributes?.length) continue;
+
+        let values: string[] = [];
+
+        if (data.costPerlevel?.length) {
+          const cost = data.costPerlevel[level - 1];
+          if (cost) {
+            values = [
+              ...values,
+              `Cost: ${cost} Skill Points`,
+            ];
+          }
+        }
+
+        values = [
+          ...values,
+          ...attributes.map(({ name, value, isPercentBased, internalName, operation }) => {
+            if (name && value) {
+              const formattedValue = isPercentBased ? `${Math.abs(value * 100).toFixed(2)}%` : value.toString();
+              const sign = value < 0 ? '-' : '';
+              return `${name}: ${sign}${formattedValue}${operation && internalName ? `\n> ${internalName} - ${operation}` : ''}`;
+            }
+            return '';
+          }),
+        ];
+
+        values = values.filter(Boolean);
+
+        const levelRequirement = data.levelRequirements?.[level - 1];
+
+        const value = [unorderedList(values), levelRequirement ? subtext(levelRequirement) : '']
+          .filter(Boolean)
+          .join('\n');
+
+        if (values.length) {
+          fields.push({
+            name: `Level ${level}`,
+            value,
+          });
+        }
+      }
+    }
+
+    embed.addFields(fields);
+
+    await interaction.editReply({
+      embeds: [embed],
+      components: [actionRow],
+    });
+  }
+
+  override async autocomplete(interaction: AutocompleteInteraction): Promise<void> {
+    const value = interaction.options.getFocused();
+
+    // TODO: Add type checking
+    let data = await api.search(value, ['Skills']);
+
+    data = data.slice(0, 25);
+
+    await interaction.respond(
+      data
+        .filter((entry) => entry.name !== undefined && entry.path !== undefined)
+        .map((entry) => ({
+          name: entry.name as string,
+          value: entry.path as string,
+        }))
+    );
+  }
+})();
