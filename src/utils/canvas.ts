810081a73173d85ed7d6ca608e28cc762a7d24a7
glazk0 <43091603+glazk0@users.noreply.github.com>
2025-06-07 16:39:07 +0200

    fix: removed text

--- src/utils/canvas.ts
+++ src/utils/canvas.ts
@@ -9,23 +9,16 @@ export const createColorPaletteImage = (colors: string[] | null | undefined): Bu
   const gap = 20;
 
   const width = colors.length * squareSize + (colors.length - 1) * gap;
-  const height = squareSize + 30;
+  const height = squareSize;
 
   const canvas = createCanvas(width, height);
   const ctx = canvas.getContext('2d');
-
-  ctx.font = 'bold 14px sans-serif';
-  ctx.textAlign = 'center';
-  ctx.textBaseline = 'top';
 
   colors.forEach((hex, index) => {
     const x = index * (squareSize + gap);
 
     ctx.fillStyle = hex;
     ctx.fillRect(x, 0, squareSize, squareSize);
-
-    ctx.fillStyle = 'white';
-    ctx.fillText(hex, x + squareSize / 2, squareSize + 10);
   });
 
   return canvas.toBuffer('image/png');
