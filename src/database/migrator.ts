270121d14ab48a22d9e5af3dbcd4dcc78ff7a196
glazk0 <43091603+glazk0@users.noreply.github.com>
2025-06-08 00:20:56 +0200

    fix: drizzle prod

--- src/database/migrator.ts
+++ src/database/migrator.ts
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const migrationConfig: MigrationConfig = {
-	migrationsFolder: join(__dirname),
+	migrationsFolder: join(__dirname, 'migrations'),
 	migrationsTable: '_migrations'
 };
 
