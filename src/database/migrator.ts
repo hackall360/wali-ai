034d9505777ed84106f2036082cd74bcc95e216b
glazk0 <43091603+glazk0@users.noreply.github.com>
2025-06-08 00:40:54 +0200

    feat: depends on & migrator

--- src/database/migrator.ts
+++ src/database/migrator.ts
@@ -1,9 +1,15 @@
 import 'dotenv/config';
 
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 import type { MigrationConfig } from 'drizzle-orm/migrator';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { migrate } from 'drizzle-orm/node-postgres/migrator';
 import pg from 'pg';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 if (!process.env.DATABASE_URL) {
 	throw new Error('DATABASE_URL is not defined in .env');
@@ -14,7 +20,7 @@ const migrationClient = new pg.Client({
 });
 
 const migrationConfig: MigrationConfig = {
-	migrationsFolder: 'migrations',
+	migrationsFolder: join(__dirname, 'migrations'),
 };
 
 export const migrateDatabase = () => {
