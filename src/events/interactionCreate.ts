5ddd3f48da10832123668a5fe10dac4e5cd98407
glazk0 <43091603+glazk0@users.noreply.github.com>
2025-07-25 06:54:45 +0200

    fix: removed entitlements

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -83,47 +83,47 @@ export default new (class extends Event {
     try {
       await command.execute(interaction, context);
 
-      const response = await fetch(`https://entitlements.glazk0.dev/applications/${interaction.client.application.id}/entitlements`, {
-        headers: {
-          'Authorization': config.entitlementsSecretToken,
-        },
-      });
+      // const response = await fetch(`https://entitlements.glazk0.dev/applications/${interaction.client.application.id}/entitlements`, {
+      //   headers: {
+      //     'Authorization': config.entitlementsSecretToken,
+      //   },
+      // });
 
-      const entitlements = await response.json() as APIEntitlement[];
+      // const entitlements = await response.json() as APIEntitlement[];
 
-      const isContributor = entitlements.some((entitlement) => entitlement.user_id === interaction.user.id);
+      // const isContributor = entitlements.some((entitlement) => entitlement.user_id === interaction.user.id);
 
-      if (interaction.commandName !== 'supporters' && !contributors.has(interaction.user.id) && !isContributor) {
-        const response = await fetch(`https://entitlements.glazk0.dev/applications/${interaction.client.application.id}/skus`, {
-          headers: {
-            'Authorization': config.entitlementsSecretToken,
-          },
-        });
+      // if (interaction.commandName !== 'supporters' && !contributors.has(interaction.user.id) && !isContributor) {
+      //   const response = await fetch(`https://entitlements.glazk0.dev/applications/${interaction.client.application.id}/skus`, {
+      //     headers: {
+      //       'Authorization': config.entitlementsSecretToken,
+      //     },
+      //   });
 
-        const skus = await response.json() as APISKU[];
+      //   const skus = await response.json() as APISKU[];
 
-        contributors.set(interaction.user.id, new Date());
+      //   contributors.set(interaction.user.id, new Date());
 
         const embed = new Embed()
           .setTitle(`Hey ${interaction.user.username}, thanks for using ${interaction.client.user.username}!`)
           .setDescription(`I hope you're enjoying the bot! If you find it helpful, please consider ${bold('supporting the developer')} to help him maintain and improve it further. Your support is greatly appreciated!`)
           .setColor(Math.floor(Math.random() * 16777215));
 
-        const actionRow = new ActionRowBuilder<ButtonBuilder>();
+      //   const actionRow = new ActionRowBuilder<ButtonBuilder>();
 
-        for (const sku of skus.filter(sku => sku.flags === (SKUFlags.UserSubscription | SKUFlags.Available))) {
-          const button = new ButtonBuilder()
-            .setStyle(ButtonStyle.Premium)
-            .setSKUId(sku.id);
-          actionRow.addComponents(button);
-        }
+      //   for (const sku of skus.filter(sku => sku.flags === (SKUFlags.UserSubscription | SKUFlags.Available))) {
+      //     const button = new ButtonBuilder()
+      //       .setStyle(ButtonStyle.Premium)
+      //       .setSKUId(sku.id);
+      //     actionRow.addComponents(button);
+      //   }
 
         await interaction.followUp({
           embeds: [embed],
-          components: actionRow.components.length ? [actionRow] : [],
+          // components: actionRow.components.length ? [actionRow] : [],
           flags: MessageFlags.Ephemeral,
         });
-      }
+      // }
 
       commandSuccessCounter.inc({
         commandType: interaction.commandType,
